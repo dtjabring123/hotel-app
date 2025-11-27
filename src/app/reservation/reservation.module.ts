21fde8370bc62e857a78cbb51b3c68100ddfa098
Dylan Tjabring <tjabringd@aforbes.com>
2025-11-27 10:21:07 +0200

    Update Reservation

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ReservationFormComponent, ReservationListComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule,
+  ],
 })
 export class ReservationModule {}
