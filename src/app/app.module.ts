a7e521b988599ab56ec4c50c6aa98cabfc5d2c60
Dylan Tjabring <tjabringd@aforbes.com>
2025-12-29 10:41:40 +0200

    Updated CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { HomeModule } from './home/home.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReservationModule } from './reservation/reservation.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ReservationModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
